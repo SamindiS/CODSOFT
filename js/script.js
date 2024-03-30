a6ef3c21739f888bc140b77ee9dbc93b2ec24723
SamindiS <samindisankalpani@gmail.com>
2024-03-30 19:41:47 +0530

    Task2

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// JavaScript for left side arrow scrolling
+document.getElementById('left-arrow').addEventListener('click', function() {
+    document.querySelector('.popular-products').scrollLeft -= 300; // Adjust scroll distance as needed
+});
+
+// JavaScript for search button
+document.querySelector('.search-button').addEventListener('click', function() {
+    // Add your search functionality here
+});
+
+// JavaScript for cart button
+document.querySelector('.cart-button').addEventListener('click', function() {
+    // Add your cart functionality here
+});
+
+// JavaScript for profile photo
+document.querySelector('.profile-photo').addEventListener('click', function() {
+    // Add your profile functionality here
+});
+
+// JavaScript for background color changing button
+document.querySelector('.background-change-button').addEventListener('click', function() {
+    // Add your background color changing functionality here
+});
